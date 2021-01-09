ac44b16747e43411935dc7e01bdc790baf5cbace
ALIK MOLDOVANU <al1k24@icloud.com>
2021-01-09 19:49:00 +0200

    Fetch data

--- API/Screens/People/PeopleTableViewController.swift
+++ API/Screens/People/PeopleTableViewController.swift
@@ -14,15 +14,53 @@ class PeopleTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        PeopleManager.shared.fetchPeople(byPage: 1) { [unowned self] (model: [PeopleResult]?) in
-            guard let people = model else {
+        setupTableCell()
+        
+        fetchData()
+    }
+    
+    private func fetchData() {
+        PeopleManager.shared.fetchPeople(byPage: 1) { [unowned self] (resultsModel: [PeopleResult]?) in
+            guard let peopleModels = resultsModel else {
                 return
             }
             
+            for model in peopleModels {
+                addOrUpdateDataToDatabase(with: model)
+            }
+            
+            let realmPeople: [PeopleRealmModel] = DatabaseManager.shared.fetch()
+            let people: [PeopleResult] = realmPeople.map { PeopleResult(realmModel: $0) }
+
             self.people = people
             self.tableView.reloadData()
         }
+    }
+    
+    private func addOrUpdateDataToDatabase(with model: PeopleResult) {
+        let peopleRealmModel: PeopleRealmModel = {
+            let realmModel = PeopleRealmModel()
+            
+            realmModel.name = model.name ?? ""
+            realmModel.height = model.height ?? ""
+            realmModel.mass = model.mass ?? ""
+            realmModel.hairColor = model.hairColor ?? ""
+            realmModel.skinColor = model.skinColor ?? ""
+            realmModel.eyeColor = model.eyeColor ?? ""
+            realmModel.birthYear = model.birthYear ?? ""
+            realmModel.gender = model.gender ?? ""
+            realmModel.homeworld = model.homeworld ?? ""
+            realmModel.created = model.created ?? ""
+            realmModel.edited = model.edited ?? ""
+            realmModel.url = model.url ?? ""
+            
+            return realmModel
+        }()
         
+        DatabaseManager.shared.add(peopleRealmModel)
+    }
+    
+    private func setupTableCell() {
         tableView.register(PeopleTableViewCell.nib, forCellReuseIdentifier: PeopleTableViewCell.cellId)
     }
 
@@ -33,11 +71,11 @@ class PeopleTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: PeopleTableViewCell.cellId, for: indexPath) as? PeopleTableViewCell, let name = people[indexPath.row].name else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PeopleTableViewCell.cellId, for: indexPath) as? PeopleTableViewCell else {
             return UITableViewCell()
         }
         
-        cell.nameLabel.text = name
+        cell.configure(with: people[indexPath.row])
         
         return cell
     }
